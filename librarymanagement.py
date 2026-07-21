49c8d5f163b283f9bd41ba04779a25112704e68b
nahor abi <nahorabi73@gmail.com>
2026-07-21 17:48:43 +0300

    added view book,search book...options

--- librarymanagement.py
+++ librarymanagement.py
@@ -3,15 +3,14 @@ books = {
     "Atomic Habits": {
         "author": "James Clear",
         "year": 2018,
-        "available": True,
-        "borrowed_by": None
-    },
+        "available": False,
+        "borrowed_by": 'nahor'
+    }
 }
 members  = {
-    # "Nahor": {
-    #     "borrowed": ["Atomic Habits" ]
-    # }
+    
 }
+print(members)
 
 def view_book():
     if not books:
@@ -20,11 +19,17 @@ def view_book():
         for i,(book, descriptions )in enumerate(books.items(), 1):
             print(f'{i}. {book}')
             print()
-            for key,value in descriptions.items():
-                print(f'{key}:{value}')
-                print()
-        
-        
+            print(f'author:{descriptions['author']}')
+            print(f'year:{descriptions['year']}')
+            
+            if descriptions['borrowed_by']:
+                if not descriptions['available'] :
+                    print(f'status:borrowed') 
+                print(f'borrowed_by:{descriptions['borrowed_by']}')
+            else:
+                if descriptions['available'] :
+                    print(f'status:available') 
+                       
 def add_book():
     current_year = 2026
     book_title = input('Book title: ').title().strip()
@@ -54,7 +59,74 @@ def add_book():
         print('this book already exist! ')
         return
 
+def search_book():
+    search = input('Enter book title or author: ').strip().lower()
+    
 
+    found = False
+
+    for book in books:
+        if search in book.lower() or search in books[book]["author"].lower():
+            view_book()
+            found = True
+
+    if not found:
+        print('This book does not exist.')
+    
+def register_member():
+
+    members_name = input('Enter member name: ').title().strip()
+    if members_name in members:
+        print('this name already exist')
+        return
+    
+    else:
+        
+        members_phone = input('Enter phone: ')
+        if isinstance(members_phone, int) or members_phone[0] =='+' :
+        
+            members[members_name] = {
+                'phone' :members_phone ,
+                'borrowed_books':[]
+            }
+            
+            print('Member registered successfully!')
+        else:
+            print('phone number only contain numbers data type')
+            return
+    
+        
+  
+def view_members():
+    if not members:
+        print('there\'s no member')
+        return
+    
+    for i,(name,properties) in enumerate(members.items(), 1):
+        print(f'{i}. {name}')
+        print(f'phone : {properties['phone']}')
+        print(f'Borrowed_books : {len(properties['borrowed_books'])}')
+        print()
+        
+           
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 def main():
     while True:
         print('==============================\nLIBRARY MANAGEMENT SYSTEM\n==============================\n1. View Books \n2. Add Book\n3. Remove Book\n4. Search Book\n5. Register Member\n6. Borrow Book\n7. Return Book\n8. View Members\n9. View Borrowed Books\n10. Library Statistics\n11. Exit')
@@ -68,6 +140,12 @@ def main():
             view_book()
         elif options ==2:
             add_book()
+        elif options ==4:
+            search_book()
+        elif options ==5:
+            register_member()
+        elif options ==8:
+            view_members()
         elif options == 11:
             print('thank you')
             break    
--- oop/main5.py
+++
@@ -1,3 +0,0 @@
-# abstract class = a class that cannot be instantiated on its own ; meant to be subclassed they can contain abstract methods , which are declared but have no implementation . abstract class benefits :
-# 1 prevent instantiation of the class itself 
-# 2 requires children to use inherited abstract methods 
